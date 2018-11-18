945fe9d8323f6a2a2ad59e8e616085cc89e5331e
Dayoung <cdypro0510@gmail.com>
2018-11-18 15:02:08 +0900

    update contents

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vue.js Resume
+
+> resume template
+
+https://github.com/KimWooHyun/resume
+
+## url
+> https://cdy0510.github.io/resume/
---
+++ src/contents.js
@@ -0,0 +1,278 @@
+module.exports = {
+    profile: {
+        name: '조다영',
+        description: 'Back-end developer',
+        dateOfBirth: '1993.05.10',
+        email: 'cdypro0510@gmail.com',
+        links: [
+            {name: 'github', href: 'https://github.com/cdy0510'},
+            {name: 'blog', href: 'https://cdy0510.github.io'}
+        ]
+    },
+    introduction: `
+2015년부터 Back-End 개발과 서버관리를 하고 있는 조다영입니다.</br>
+현재 사용하고 있는 기술스택은 JAVA - Spring이며, 서브로 node.js - express를 이용해 채팅 서비스를 개발 했습니다. </br>
+최근 사용했던 서버는 AWS - ubuntu 환경에서 개발했으며, 이전 직장에서는 Windows서버를 사용했습니다.</br>
+AWS를 이용하다보니 서버관련 서비스들에 관심이 생겨 AWS 서비스를 하나씩 경험해보고 있습니다. 예를 들어 serverless를 경험해보기 위해 썸네일을 자동으로 생성해주는 Lambda 함수를 작성해보기도 하고, 음성SNS를 개발할 때 CloudFront로 S3에 있는 음성파일을 CDN으로 스트리밍할 수 있게 구성했으며, CloudWatch를 통해 서버를 모니터링하기도 했습니다. 채팅서비스를 개발할 땐 Elastic Beanstalk으로 자동 배포되는 환경을 구축해보았는데 기존 배포환경과 비교해 빠르고 번거롭지 않게 배포할 수 있는 점이 좋았습니다. 이 외에도 다양한 서비스를 이용해 쉽게 프로젝트에서 원하는 기능들을 추가했습니다. AWS의 장점은 내가 원하는 서비스를 언제든지 추가하고 서버 사양을 바꿀 수 있기 때문에 제가 개발하고 있는 서비스를 운영하기에 적절했습니다. 또한 GCP를 실습해보는 스터디에 참여하여 AWS외에도 다양한 서버환경을 배우기 위해 노력했습니다.
+Back-End뿐만 아니라, 사용자에게 직접 노출되는 UI/UX에도 관심이 많아 사내에서 퍼블리싱과 디자인기획에도 참여했습니다.</br>
+기술 세미나참여와 커뮤니티 활동을 좋아해 활동하고 있는 커뮤니티 내에서 Python - Django 스터디를 주도해 진행하기도 하고, 2017년 말부터 DjangoGirls Seoul의 Organizer로 활동하며 다양한 기술 행사를 기획/진행하고 있습니다. 2018년 11월부터는 GDG Organizer들의 요청에따라 Google Developers Groups의 Seoul챕터에서 함께 운영을 하고 있습니다.</br></br>
+저의 강점은 추진력이 강하다는 것입니다. 첫번째 회사에서 몇년간 미뤄두었던 개선 작업들을 빠르게 처리하다보니 회사에서 최초로 '집중 근무일'제도를 만들어 일주일 중 하루를 개선 작업에 몰두 할 수 있도록 재택근무의 기회와 시간을 주시기도 했습니다. 두번째 회사에서는 AWS의 다양한 기능들을 하나씩 도입하여 업무 효율도 높이고, 기능적으로도 안정적인 서비스를 제공하기 위해 노력했습니다.</br></br>
+현실에 안주하는 것이 아니라 매일 조금씩이라도 발전하자는 모토를 가지고 제가 사용하는 기술들에 대해 공부하려고 노력합니다. 처음에는 기술 세미나나 커뮤니티 활동을 하며 들었던 기술에 대해 검색을 해보기도 하고, 실제 제가 하고 있는 프로젝트에 도입할 수 있는 부분이 없는지 찾는 편입니다. 글로 읽는 것보단 실제 적용해보며 배우는 것이 훨씬 많기 때문입니다. 최근에는 개발방법론에 관심이 생겨 켄트백저자의 TDD책을 읽으며 테스트주도개발을 실습해보기도 했습니다. 테스트 코드의 필요성과 이점을 경험했기 때문에 테스트코드를 작성하는 문화를 가진 회사에 관심을 가지고 있습니다.
+    `
+    ,
+    contents: {
+        'Skill Set': [
+            {
+                'date': 'Language',
+                'title': ' Java / JSP / node.js / ASP / JavaScript / Python3',
+                'contents': ['']
+            },
+            {
+                'date': 'Framework',
+                'title': 'Spring / Express / Django',
+                'contents': ['']
+            },
+            {
+                'date': 'Web Server',
+                'title': 'Apache / Nginx / Tomcat',
+                'contents': ['']
+            },
+            {
+                'date': 'DB',
+                'title': 'MySQL / MSSQL',
+                'contents': ['']
+            },
+            {
+                'date': 'Cloud',
+                'title': 'AWS',
+                'contents': ['']
+            }
+        ],
+        '개인 활동': [
+            {
+                'date': '2017.10 - 현재',
+                'title': 'Django Girls Seoul',
+                'contents': [
+                    '<span style="font-weight: bold;">Django Girls Seoul Organizer</span>',
+                    '- 행사기획&운영',
+                    '- 홍보글 작성',
+                    '- <a href="https://www.facebook.com/djangogirlsseoul">장고걸스 페이스북 페이지</a>'
+                ]
+            },
+            {
+                'date': '2018.11 - 현재',
+                'title': 'Google Develpers Groups',
+                'contents': [
+                    '<span style="font-weight: bold;">GDG Seoul Organizer</span>',
+                    '- 행사기획&운영',
+                    '<a href="https://www.facebook.com/groups/gdgseoul/">GDG Seoul 페이스북 페이지</a>',
+                ]
+            },
+        ],
+        '학력 사항': [
+            {
+                'date': '2015.03 - 2017.02',
+                'title': '한양사이버대학교 졸업',
+                'contents': ['컴퓨터공학과', '2015년도 편입']
+            },
+            {
+                'date': '2012.03 - 2015.02',
+                'title': '한양여자대학교 졸업',
+                'contents': ['컴퓨터정보과', '졸업작품: 개인 맞춤 도시락 사이트 개발']
+            },
+        ],
+        '자격증': [
+            {
+                'date': '2016.07',
+                'title': '정보처리기사',
+                'contents': ['한국산업인력공단']
+            },
+            {
+                'date': '2015.07',
+                'title': '정보처리산업기사',
+                'contents': ['한국산업인력공단']
+            },
+        ]
+    },
+    summary: {
+        '경력 요약': [
+            {
+                'date': ['2017.06', '2018.06'],
+                'title': '(주)위드마인드',
+                'contents': [
+                    '서버개발자',
+                    '주 - Back-end 개발',
+                    '서브 - 퍼블리싱'
+                ]
+            },
+            {
+                'date': ['2015.01', '2016.11'],
+                'title': '(주)웹커뮤니티',
+                'contents': [
+                    '개발팀 - 사원',
+                    '주 - Back-end 개발',
+                    '서브 - 퍼블리싱, Android App 개발'
+                ]
+            }
+        ]
+    },
+    detailContents: {
+        '(주)위드마인드': {
+            'date': '2017.06 - 2018.06',
+            'info': '음성데이터를 이용하여 감정을 분석하는 서비스를 개발중인 스타트업',
+            'service': [
+                '음성데이터를 감정데이터로 변환하는 API제공',
+                '상담센터 관리 페이지 제작',
+                '외주 프로젝트 진행중',
+            ],
+            'project': {
+                'project': [
+                    {
+                        'date': '2018.04 - 2018.05',
+                        'title': '웹앱 퍼블리싱',
+                        'contents': [
+                            '미완성 된 웹앱 페이지 마무리 작업',
+                            '<div style="background: #F3F3F3;border: 1px dashed #DDD; padding: 10px;">[Stack]<br>HTML, CSS, BootStrap, Jquery, Javascript</div>'
+                        ]
+                    },
+                    {
+                        'date': '2017.08 - 2018.04',
+                        'title': '음성 SNS 개발',
+                        'contents': [
+                            '관리자 페이지 개발',
+                            '음성 SNS APP API개발',
+                            '<br />',
+                            '퍼블리싱 외 모든 개발 단독 진행',
+                            '<br />',
+                            '음성 파일 기반의 SNS서비스',
+                            'FCM으로 Push기능 제공',
+                            '사용자가 설정한 감정아이콘에 따라 평균 감정 데이터를 관리자 페이지에서 보여주고, 이 데이터로 다른 사용자와 매칭해주는 서비스를 제공',
+                            'STT(Speech To Text)와 번역API를 이용하여 음성 파일과 함께 번역된 텍스트를 제공',
+                            '',
+                            '<div style="background: #F3F3F3;border: 1px dashed #DDD; padding: 10px;">[Stack]<br>JAVA, Spring, AWS, GCP, MySQL</div>'
+                        ]
+                    },
+                    {
+                        'date': '2017.10 - 2018.01',
+                        'title': '심리상담소 관련 개발',
+                        'contents': [
+                            '심리상담소 관리 페이지',
+                            '상담사 페이지(내담자 관리와 채팅기능)',
+                            '내담자 APP API개발',
+                            '<br />',
+                            '퍼블리싱 외 모든 개발 단독 진행',
+                            '<br />',
+                            '[심리상담소 관리 페이지] - Java, Spring, MySQL, AWS',
+                            '상담사 관리 기능 - 채팅관리 및 월별 상담만족도 그래프표시(Google Charts)',
+                            '회원 관리 - 회원 정보관리',
+                            'APP 관리 - 푸시 및 팝업등 관리',
+                            '게시판 관리',
+                            '컨텐츠 관리 - 앱에 표시되는 컨텐츠 등록 및 수정(피키캐스트와 비슷한 형태)',
+                            '<br />',
+                            '[상담사 페이지] - Node.js, Express, MySQL, AWS',
+                            'Socket.io를 이용하여 채팅 서비스 구현',
+                            '파일 및 동영상 업로드, 읽음 표시, 실시간 채팅 구현',
+                            '채팅관리 및 월별 상담만족도 그래프표시(Google Charts)',
+                            '<br />',
+                            '[API] - Java, Spring, MySQL, AWS',
+                            '관리자가 등록한 컨텐츠 제공(피키캐스트와 비슷한 형태)',
+                            '컨텐츠에 따른 추천 상담사 기능제공',
+                            '파일 및 동영상 업로드, 읽음 표시, 실시간 채팅 구현',
+                            '<div style="background: #F3F3F3;border: 1px dashed #DDD; padding: 10px;">[Stack]<br>JAVA, Spring, Node.js, Express, AWS, MySQL</div>'
+                        ]
+                    },
+                    {
+                        'date': '2017.06 - 2018.07',
+                        'title': '위드마인드 상담센터 관리 개발',
+                        'contents': [
+                            '퍼블리싱 외 모든 개발 단독 진행',
+                            '<br />',
+                            '상담센터 통합관리 페이지 개발',
+                            '상담자용 내담자 관리 페이지 개발',
+                            'ASP로 제작된 소스를 JAVA로 변환(음성데이터를 감정데이터로 변환하는 부분)',
+                            '위드마인드 앱과 연동되는 API개발',
+                            '상담사는 내담자 데이터를 참고하며 상담일지를 작성할 수 있음',
+                            '내담자의 감정 정보를 그래프로 제공(Google Charts)',
+                            '<div style="background: #F3F3F3;border: 1px dashed #DDD; padding: 10px;">[Stack]<br>JAVA, Spring, AWS, MySQL</div>'
+                        ]
+                    },
+                ]
+            }
+        },
+        '(주)웹커뮤니티': {
+            'date': '2015.01 - 2016.11',
+            'info': '교회 홈페이지 전문 제작 업체',
+            'service': [
+                '<a href="http://www.webchurch.co.kr/">http://www.webchurch.co.kr/</a>',
+                '교회 홈페이지 제작 및 관리자용 툴 제공',
+                '교회 어플 제작(웹앱)',
+                '실시간 설교 스트리밍 서비스 제공',
+                '설교 및 찬양영상 등 동영상 전용 게시판 제공',
+                '문자 서비스 제공',
+                '맞춤형 사이트 제작',
+                '<div style="background: #F3F3F3;border: 1px dashed #DDD; padding: 10px;">[Stack]<br>ASP, MSSQL, Windows Server, HTML, CSS, Javascript, BootStrap, Jquery</div>'
+            ],
+            'project': {
+                'project': [
+                    {
+                        'date': '',
+                        'title': '사이트 속도 개선',
+                        'contents': [
+                            '쿼리문 수정 및 인덱싱 추가 - 특히 사내 관리시스템 속도 개선에 큰 효과가 있었음',
+                            '이미지 자동 썸네일 추출 작업을 통해 첫페이지 로딩 속도 개선',
+                            '프레임으로 감싸져 있는 소스를 div형태로 변경하며 코드 리팩토링 진행',
+                        ]
+                    }, {
+                        'date': '',
+                        'title': 'Paypal 연동 및 실시간 환율 반영',
+                        'contents': [
+                            '해외 교회에서 결제 시 입금이 어려운 부분이 있어 Paypal 서비스를 도입',
+                            '하나은행에서 제공하는 사이트를 크롤링하여 그날의 환율을 반영하여 결제할 수 있도록 함',
+                        ]
+                    }, {
+                        'date': '',
+                        'title': '자동화 시스템 구축',
+                        'contents': [
+                            '교회 홈페이지 생성 시 필요한 일련의 작업을 클릭 한번으로 구축될 수 있도록 함',
+                            '간단한 작업임에도 자동화 되지 않아 직접 DB에서 쿼리를 날리거나, 서버에 접근해야 했던 작업을 자동화 시킴으로 기획팀에서도 쉽게 작업이 가능하도록하여 작업 처리 속도가 빨라짐',
+                            'ex) 교회 생성작업, 게시판 생성작업 등',
+                        ]
+                    }, {
+                        'date': '',
+                        'title': 'VBScript를 Javascript로 변환',
+                        'contents': [
+                            'VBScript를 Javascript로 변환하여 오래된 소스에서 동작하지 않았던 오류들을 수정함.',
+                        ]
+                    }, {
+                        'date': '',
+                        'title': '서버관리',
+                        'contents': [
+                            '신규 고객 홈페이지 셋팅(IIS)',
+                            '네임서버와 동영상 서버 셋팅',
+                            '서버 모니터링',
+                            '스케쥴링된 작업 관리',
+                        ]
+                    }, {
+                        'date': '',
+                        'title': '고객 편의 시스템 구축',
+                        'contents': [
+                            'CRUD기반의 솔루션 제작을 통해 고객이 쉽게 홈페이지를 관리할 수 있도록 함',
+                            '온라인 견적 서비스를 구축하여 PDF로 받아 볼 수 있도록 함',
+                            '단문 문자만 가능했던 문자서비스를 LMS로 확대하여 문자이용률을 증가시킴',
+                            '업무시간 외 고객 사이트의 오류사항 접수 시 담당 직원에게 문자가 가도록하여 빠르게 처리할 수 있도록 함',
+                            '동일한 오류사항에 대하여 바로 대응할 수 있도록 오류관리 서비스를 사내 사이트에 구축하여 업무 효율성을 높임',
+                        ]
+                    }, {
+                        'date': '',
+                        'title': 'Android/iOS 웹앱 제작 및 유지보수',
+                        'contents': [
+                            '모바일 전용 웹사이트 제작 후 껍데기를 씌워 배포',
+                            '카카오톡 친추하기 기능 추가',
+                            '푸시 기능 추가',
+                        ]
+                    },
+                ]
+            }
+        },
+    }
+};
